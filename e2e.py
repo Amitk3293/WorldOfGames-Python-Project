b67fceabfd6ddade240300b4af30b52707df6f97
root <root@pop-os.localdomain>
2021-10-20 16:11:12 +0200

    e2e changed to headless

--- e2e.py
+++ e2e.py
@@ -1,9 +1,12 @@
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
 import os
 
 def test_scores_archive():
     global scoreid
-    driver = webdriver.Firefox()
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
     driver.get('http://127.0.0.1:5000')
     scoreid = driver.find_element_by_id("score").text
     if 0 < int(scoreid) < 1000:
